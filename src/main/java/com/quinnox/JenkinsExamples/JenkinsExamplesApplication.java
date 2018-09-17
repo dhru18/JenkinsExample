dde2bb5f5808e6fc3b5521019c07282738a58978
Dhrumil shah <dhru18shah@gmail.com>
2018-09-17 02:50:24 -0700

    testing

---
+++ src/main/java/com/quinnox/JenkinsExamples/JenkinsExamplesApplication.java
@@ -0,0 +1,12 @@
+package com.quinnox.JenkinsExamples;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JenkinsExamplesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JenkinsExamplesApplication.class, args);
+	}
+}
